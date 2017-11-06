40a2ff8a57fb19ef6f45cea3d6ad299cf7d891c4
Star1122 <kitdb@outlook.com>
2017-11-06 22:13:31 +0100

    Fix the routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 
 import configureStore from './common/configureStore';
 import routes from './routes';
 
 const store = configureStore();
 
-const history = syncHistoryWithStore(browserHistory, store);
+const history = createHistory();
+
+// Get the current location.
+const location = history.location;
+
+// Listen for changes to the current location.
+const unlisten = history.listen((location, action) => {
+  // location is an object like window.location
+  console.log(action, location.pathname, location.state)
+});
+
+// Use push, replace, and go to navigate around.
+history.push('/projects', { });
+
+// To stop listening, call the function returned from listen().
+unlisten();
 
 render(
   <Provider store={store}>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IndexRoute, Route } from 'react-router';
+import { Switch, Route } from 'react-router';
 
 import App from './containers/app/App';
 import Projects from './containers/project/Projects';
@@ -11,37 +11,19 @@ import ProjectLists from './containers/project/ProjectLists';
 import ProjectSettings from './containers/project/ProjectSettings';
 
 const routes = (
-  <Route path="/" component={App}>
-    <IndexRoute component={Projects} />
+  <Switch>
+    <Route path="/" component={App} />
 
-    <Route path="/projects/:projectId">
-      <IndexRoute component={Project} />
+    <Route path="/projects" component={Projects} />
 
-      <Route path="data" component={ProjectData}>
-        <IndexRoute component={ProjectData} />
+    <Route path="/projects/:projectId" component={Project} />
 
-        <Route
-          path="keywords"
-          component={ProjectKeywords}
-        />
-      </Route>
-
-      <Route
-        component={ProjectSegments}
-        path="/segments"
-      />
-
-      <Route
-        component={ProjectLists}
-        path="/lists"
-      />
-
-      <Route
-        component={ProjectSettings}
-        path="/settings"
-      />
-    </Route>
-  </Route>
+    <Route path="/projects/:projectId/data" component={ProjectData} />
+    <Route path="/projects/:projectId/keywords" component={ProjectKeywords} />
+    <Route path="/projects/:projectId/segments" component={ProjectSegments} />
+    <Route path="/projects/:projectId/lists" component={ProjectLists} />
+    <Route path="/projects/:projectId/settings" component={ProjectSettings} />
+  </Switch>
 );
 
 export default routes;
